86f1490f049afa022f0e50e2ad7700f37a2588ba
Karelle <karellehofler@gmail.com>
2018-03-28 12:08:05 -0400

    Documentation added to Javascript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//function to support creation of the pixels 
 $.fn.multiply = function(numCopies) {
     var newElements = this.clone();
     for(var i = 1; i < numCopies; i++)
@@ -7,8 +8,10 @@ $.fn.multiply = function(numCopies) {
     return newElements;
 };
 
+//Multiples the pixels by 1023. Total of 1024.
 $('.pixel').multiply(1023).insertAfter('.pixel');
 
+//Changes pixel default state from gray to Black
 function changePixel(){
     $(".pixel").mouseenter(function(){
         $(this).css('background-color', '#000');
@@ -16,7 +19,7 @@ function changePixel(){
 }
 
 changePixel();
-
+//Function to change color to Black
 function changetoBlack(){
     $(".black").click(function(){
         $(".pixel").mouseenter(function(){
@@ -25,6 +28,7 @@ function changetoBlack(){
     }); 
 }
 
+//Function to change color to Red
 function changetoRed(){
     $(".red").click(function(){
         $(".pixel").mouseenter(function(){
@@ -33,6 +37,7 @@ function changetoRed(){
     }); 
 }
 
+//Function to change color to Yellow
 function changetoYellow(){
     $(".yellow").click(function(){
         $(".pixel").mouseenter(function(){
@@ -41,6 +46,7 @@ function changetoYellow(){
     }); 
 }
 
+//function to change color to blue
 function changetoBlue(){
     $(".blue").click(function(){
         $(".pixel").mouseenter(function(){
@@ -49,6 +55,7 @@ function changetoBlue(){
     }); 
 }
 
+//Resets entire pad and pixels to original state
 function resetPad(){
     $('.reset').click(function() {
         $('.pixel').css({'background-color': '#CCC'});
